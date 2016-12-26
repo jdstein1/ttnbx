adb7cce6fc7c89b8a71d133ecbf1de3601ad9c83
jdstein1 <jdstein1@gmail.com>
2016-12-26 12:53:47 -0600

    optimize script.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,15 @@
 const keys = document.querySelectorAll('.key'); // cache key elements
 const audios = document.getElementsByTagName("audio"); // cache audio elements
-const codes = []; // array of codes
-
-/* push each code into codes array */
-for (let i = 0; i < audios.length; i++) {
-  codes.push(parseInt(audios[i].dataset.key));
-}
-
-/* get index of key with specific code */
-function isMyKey(arr,key,val) {
-  for (let i = 0; i < arr.length; i++) {
-    const myCode = parseInt(arr[i].dataset[key]);
-    if (myCode === val) {
-      const index = i;
-      return index;
-    }
-  }
-}
 
 /* play audio associated w key and start CSS transition */
 function playAudio(e) {
-  // console.group('START playAudio');
-  const myKey = e.keyCode;
-  const myIndex = isMyKey(audios,"key",myKey);
-  console.log('audio played: ', keys[myIndex].children[1].innerHTML);
-  audios[myIndex].currentTime = 0;
-  audios[myIndex].play().then(function(){
-    keys[myIndex].classList.add("playing");
+  // console.group('START playAudio', e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // skip it if it is not a known key
+  audio.currentTime = 0;
+  audio.play().then(function(){
+    key.classList.add("playing");
   });
   // console.groupEnd();
 }
@@ -38,10 +21,8 @@ function undoTransition(e) {
 }
 
 /* listen for key down event */
-document.addEventListener("keydown", function(){
+window.addEventListener("keydown", function(){
   console.log('key pressed: ', event.key);
-  // play audio only if it is one of our key codes
-  if (codes.indexOf(event.keyCode) < 0) return;
   playAudio(event);
 });
 
